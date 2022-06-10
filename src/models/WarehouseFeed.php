27b54967beef3515252b4e1acc8bb28d071b7486
raidkon <n@raidkon.com>
2022-06-10 15:55:03 +0500

    Добавлены новые фиды

--- src/models/WarehouseFeed.php
+++ src/models/WarehouseFeed.php
@@ -5,7 +5,9 @@ namespace infotech\components\models;
 class WarehouseFeed
 {
     public const AUTO_RU = 1;
+    public const AUTO_RU_V2 = 18;
     public const AUTO_SPOT = 2;
+    public const AUTO_SPOT_V2 = 17;
     public const AVITO = 3;
     public const CAR_COPY = 4;
     public const CARS_GURU = 5;
@@ -16,6 +18,7 @@ class WarehouseFeed
     public const LEGO_CAR = 10;
     public const MAXPOSTER_CLOSE = 11;
     public const MAXPOSTER = 12;
+    public const MAXPOSTER_OLD = 19;
     public const RAIFFEISEN = 13;
     public const SBERBANK = 14;
     public const YANDEX_DIRECT = 15;
@@ -24,22 +27,25 @@ class WarehouseFeed
     public static function getList(): array
     {
         return [
-            self::AUTO_RU => 'AutoRu',
+            self::AUTO_RU => 'Auto.ru',
+            self::AUTO_RU_V2 => 'Auto.ru V2',
             self::AUTO_SPOT => 'AutoSpot',
-            self::AVITO => 'Avito',
-            self::CAR_COPY => 'CarCopy',
+            self::AUTO_SPOT_V2 => 'AutoSpot v.2',
+            self::AVITO => 'Auto.ru',
+            self::CAR_COPY => 'Car Copy',
             self::CARS_GURU => 'CarsGuru',
-            self::CM_EXPERT => 'CmExpert',
-            self::DROM_RU => 'DromRu',
-            self::EURO_PLAN => 'EuroPlan',
-            self::EXPERT_AVITO => 'ExpertAvito',
+            self::CM_EXPERT => 'CM.Expert',
+            self::DROM_RU => 'Drom.ru',
+            self::EURO_PLAN => 'Европлан',
+            self::EXPERT_AVITO => 'Expert Avito',
             self::LEGO_CAR => 'LegoCar',
-            self::MAXPOSTER_CLOSE => 'MaxposterClose',
-            self::MAXPOSTER => 'Maxposter',
+            self::MAXPOSTER_CLOSE => 'MaxPoster - Закрывающий',
+            self::MAXPOSTER => 'MaxPoster',
+            self::MAXPOSTER_OLD => 'MaxPoster - Старая версия',
+            self::SBERBANK => 'Sberbank Лизинг',
             self::RAIFFEISEN => 'Raiffeisen',
-            self::SBERBANK => 'Sberbank',
-            self::YANDEX_DIRECT => 'YandexDirect',
-            self::YANDEX_XML => 'YandexXml',
+            self::YANDEX_DIRECT => 'Смарт баннеры Яндекс Директ',
+            self::YANDEX_XML => 'YML',
         ];
     }
 }
