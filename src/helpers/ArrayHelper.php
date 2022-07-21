716596a0d8df23ce1ce0fab25bc7f0a25676d9e0
spyke113 <spyke.coder@gmail.com>
2022-07-21 18:39:06 +0300

    В `ExcelHelper` добавлен метод, позволяющий строить многомерные заголовки таблиц

---
+++ src/helpers/ArrayHelper.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace infotech\components\helpers;
+
+final class ArrayHelper
+{
+    /**
+     * Вычисляет глубину многомерного массива
+     *
+     * @param array $array
+     * @return int
+     */
+    public static function depth(array $array): int
+    {
+        static $depth;
+
+        $depth ??= 1;
+
+        if (self::arrayIn($array)) {
+            $depth++;
+            foreach ($array as $value) {
+                if (self::arrayIn($value)) {
+                    self::depth($value);
+                }
+            }
+        }
+
+        return $depth;
+    }
+
+    /**
+     * @param array $array
+     * @return bool
+     */
+    protected static function arrayIn(array $array): bool
+    {
+        foreach ($array as $value) {
+            if (is_array($value)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Сведение многомерного массива в один уровень
+     *
+     * @param array $array
+     * @param int   $depth
+     * @return array
+     */
+    public static function flatten(array $array, int $depth = INF): array
+    {
+        $result = [];
+
+        foreach ($array as $item) {
+            if (is_array($item)) {
+                if ($depth === 1) {
+                    $result = array_merge($result, $item);
+                    continue;
+                }
+
+                $result = array_merge($result, static::flatten($item, $depth - 1));
+                continue;
+            }
+
+            $result[] = $item;
+        }
+
+        return $result;
+    }
+}
