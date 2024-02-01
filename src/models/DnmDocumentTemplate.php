fee1c6ece7303f1571e3229834f5189152293f32
Vladislav Kazakov <kazakvvru@gmail.com>
2024-02-01 16:00:37 +0300

    issue infotech-ru/dsfsubaru#18909

---
+++ src/models/DnmDocumentTemplate.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace infotech\components\models;
+
+class DnmDocumentTemplate
+{
+    public const TYPE_CORPORATE_CLIENT_PROCUREMENT_CAR_VEHICLE = 1;
+    public const TYPE_CORPORATE_CLIENT_PROCUREMENT_CAR = 2;
+    public const TYPE_WORKSHEET_CAR_OFFER = 3;
+    public const TYPE_WORKSHEET_CAR_WAREHOUSE_VEHICLE_OFFER = 4;
+}
