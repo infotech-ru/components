0587acacac7f27c147052b850a97669e46ecfcbf
Pavel Kostychev <kostychevpavel@gmail.com>
2024-05-14 11:41:38 +0300

    #17014 Added DnmDocumentTemplate types

--- src/models/DnmDocumentTemplate.php
+++ src/models/DnmDocumentTemplate.php
@@ -8,4 +8,7 @@ class DnmDocumentTemplate
     public const TYPE_CORPORATE_CLIENT_PROCUREMENT_CAR = 2;
     public const TYPE_WORKSHEET_CAR_OFFER = 3;
     public const TYPE_WORKSHEET_CAR_WAREHOUSE_VEHICLE_OFFER = 4;
+    public const TYPE_CORPORATE_DISCOUNT = 5;
+    public const TYPE_IMPLEMENTER_INSTRUCTION = 6;
+    public const TYPE_IMPLEMENTER_REPORT = 7;
 }
