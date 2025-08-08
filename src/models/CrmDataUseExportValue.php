2f7141f83d107b6b830e51930f38a495a4602ec7
Vladislav Kazakov <kazakvvru@gmail.com>
2025-08-08 16:09:39 +0300

    issue infotech-ru/internal/issue/23

--- src/models/CrmDataUseExportValue.php
+++ src/models/CrmDataUseExportValue.php
@@ -4,93 +4,46 @@ namespace infotech\components\models;
 
 class CrmDataUseExportValue
 {
-    public const CODE_RETAIL_CASES_CREATED = 'retail_cases_created';
-    public const CODE_RETAIL_CASES_ACTIVATED = 'retail_cases_activated';
-    public const CODE_RETAIL_CASES_SENDING_SMS = 'retail_cases_sending_sms';
-    public const CODE_RETAIL_CASES_API_EVENTS = 'retail_cases_api_events';
-    public const CODE_RETAIL_CASES_OFFERS = 'retail_cases_offers';
-    public const CODE_RETAIL_CASES_SENDING_OFFERS_TO_EMAIL = 'retail_cases_sending_offers_to_email';
+    public const CODE_CRM_USES = 'crm_uses';
+    public const CODE_USERS_SCHEDULE = 'users_schedule';
+    public const CODE_WAZZUP = 'wazzup';
+    public const CODE_BACHATA = 'bachata';
+    public const CODE_COLD_CALLS = 'cold_calls';
 
-    public const CODE_RETAIL_CASES_CLOSED_SCHEDULED_STAGES = 'retail_cases_closed_scheduled_stages';
-    public const CODE_RETAIL_CASES_OVERDUE_CLOSED_SCHEDULED_STAGES = 'retail_cases_overdue_closed_scheduled_stages';
-    public const CODE_RETAIL_CASES_CLOSED_CONTRACTS = 'retail_cases_closed_contracts';
-    public const CODE_RETAIL_CASES_CLOSED_DELIVERIES = 'retail_cases_closed_deliveries';
-    public const CODE_RETAIL_CASES_CLOSED_DELIVERIES_WITH_WAREHOUSE_VEHICLE = 'retail_cases_closed_deliveries_with_warehouse_vehicle';
-    public const CODE_RETAIL_CASES_CLOSED_CONTRACTS_WITH_WAREHOUSE_VEHICLE = 'retail_cases_closed_contracts_with_warehouse_vehicle';
-    public const CODE_RETAIL_CASES_WITHOUT_DSK = 'retail_cases_without_dsk';
-    public const CODE_RETAIL_CASES_CANCEL_REASONS = 'retail_cases_cancel_reasons';
-    public const CODE_RETAIL_CASES_TASKS = 'retail_cases_tasks';
-    public const CODE_RETAIL_CASES_SALES_ROOM = 'retail_cases_sales_room';
+    public const CODE_RETAIL_CASES = 'retail_cases';
     public const CODE_RETAIL_PLANS = 'retail_plans';
-    public const CODE_RETAIL_WAREHOUSE_EXPORTS = 'retail_warehouse_exports';
-    public const CODE_RETAIL_WAREHOUSE_DELIVERIES = 'retail_warehouse_deliveries';
+    public const CODE_RETAIL_CASES_ISSUES_WITHOUT_WAREHOUSE_VEHICLES = 'retail_cases_issues_without_warehouse_vehicles';
+    public const CODE_RETAIL_CASES_OFFERS = 'retail_cases_offers';
+    public const CODE_RETAIL_CASES_1C_CONTRACTS = 'retail_cases_1c_contracts';
 
-    public const CODE_SERVICE_CASES_CREATED = 'service_cases_created';
-    public const CODE_SERVICE_CASES_ACTIVATED = 'service_cases_activated';
+    public const CODE_CORPORATE_CASES = 'corporate_cases';
+    public const CODE_CORPORATE_CASES_ISSUES_WITHOUT_WAREHOUSE_VEHICLES = 'corporate_cases_issues_without_warehouse_vehicles';
+    public const CODE_CORPORATE_CASES_OFFERS = 'corporate_cases_offers';
+    public const CODE_CORPORATE_CASES_1C_CONTRACTS = 'corporate_cases_1c_contracts';
 
-    public const CODE_BODYSHOP_CASES_CREATED = 'bodyshop_cases_created';
-    public const CODE_BODYSHOP_CASES_ACTIVATED = 'bodyshop_cases_activated';
+    public const CODE_TRADE_IN_CASES = 'trade_in_cases';
+    public const CODE_COMMISSION_CASES = 'commission_cases';
+    public const CODE_BUYOUT_CASES = 'buyout_cases';
+    public const CODE_CM_EXPERT = 'cm_expert';
+    public const CODE_MAX_POSTER = 'max_poster';
 
-    public const CODE_TRADE_IN_CASES_CREATED = 'trade_in_cases_created';
-    public const CODE_TRADE_IN_CASES_ACTIVATED = 'trade_in_cases_activated';
-
-    public const CODE_SALES_USED_CASES_CREATED = 'sales_used_cases_created';
-    public const CODE_SALES_USED_CASES_ACTIVATED = 'sales_used_cases_activated';
+    public const CODE_SALES_USED_CASES = 'sales_used_cases';
     public const CODE_SALES_USED_PLANS = 'sales_used_plans';
-    public const CODE_SALES_USED_WAREHOUSE_EXPORTS = 'sales_used_warehouse_exports';
-    public const CODE_SALES_USED_WAREHOUSE_DELIVERIES = 'sales_used_warehouse_deliveries';
-    public const CODE_SALES_USED_CASES_CLOSED_DELIVERIES = 'sales_used_cases_closed_deliveries';
 
-    public const CODE_CREDIT_INSURANCE_CASES_CREATED = 'credit_insurance_cases_created';
-    public const CODE_CREDIT_INSURANCE_CASES_ACTIVATED = 'credit_insurance_cases_activated';
-    public const CODE_CREDIT_INSURANCE_CASES_WITHOUT_DSK = 'credit_insurance_cases_without_dsk';
+    public const CODE_CREDIT_INSURANCE_CASES = 'credit_insurance_cases';
 
-    public const CODE_PREPARED_SERVICE_CASES_CREATED = 'prepared_service_cases_created';
-    public const CODE_PREPARED_SERVICE_CASES_ACTIVATED = 'prepared_service_cases_activated';
-
-    public const CODE_COMPLAINT_CASES_CREATED = 'complaint_cases_created';
-    public const CODE_COMPLAINT_CASES_ACTIVATED = 'complaint_cases_activated';
-
-    public const CODE_HAS_DOCUMENT_TEMPLATES = 'has_document_templates';
-    public const CODE_HAS_STAGE_RULES = 'has_stage_rules';
-    public const CODE_HAS_CM_EXPERT = 'has_cm_expert';
-    public const CODE_CM_EXPERT_EXPORT_WAREHOUSE_VEHICLES = 'cm_expert_export_warehouse_vehicles';
-    public const CODE_HAS_MAX_POSTER = 'has_max_poster';
-    public const CODE_HAS_SERVICE_RECORDING = 'has_service_recording';
+    public const CODE_PREPARED_SERVICE_CASES = 'prepared_service_cases';
+    public const CODE_SERVICE_CASES = 'service_cases';
+    public const CODE_SERVICE_CASE_SCHEDULE = 'service_case_schedule';
     public const CODE_PURCHASES = 'purchases';
-    public const CODE_VEHICLE_RECOMMENDATIONS = 'vehicle_recommendations';
-    public const CODE_VEHICLE_RECOMMENDATIONS_CONTACTS = 'vehicle_recommendations_contacts';
-    public const CODE_CLIENT_VIEW_CASES = 'client_view_cases';
+
+    public const CODE_BODY_SHOP_CASES = 'body_shop_cases';
     public const CODE_TASK_BOARD_POSITIONS = 'task_board_positions';
 
-    public const CODE_RECEPTION_CASES = 'reception_cases';
-    public const CODE_RECEPTION_SECONDARY_STAGES = 'reception_secondary_stages';
+    public const CODE_RETAIL_WAREHOUSE_EXPORTS = 'retail_warehouse_exports';
+    public const CODE_CM_EXPERT_RETAIL_WAREHOUSE = 'code_cm_expert_retail_warehouse';
 
-    public const CODE_CALLS = 'calls';
-    public const CODE_CALLS_WITH_CASES = 'calls_with_cases';
-    public const CODE_WAZZUP = 'wazzup';
-
-    public const CODE_MARKETING_YEAR_REQUESTS = 'marketing_year_requests';
-    public const CODE_MARKETING_QUARTER_REQUESTS = 'marketing_quarter_requests';
-    public const CODE_MARKETING_PLACINGS = 'marketing_placings';
-
-    public const CODE_CALLTOUCH_CACHE = 'calltouch_cache';
-    public const CODE_NPS_TYPES = 'nps_types';
-
-    public const CODE_RETAIL_CASES_POLLS = 'retail_cases_polls';
-    public const CODE_SALES_USED_CASES_POLLS = 'sales_used_cases_polls';
-    public const CODE_CREDIT_INSURANCE_CASES_POLLS = 'credit_insurance_cases_polls';
-    public const CODE_PREPARED_SERVICE_CASES_POLLS = 'prepared_service_cases_polls';
-    public const CODE_SERVICE_CASES_POLLS = 'service_cases_polls';
-    public const CODE_BODYSHOP_CASES_POLLS = 'bodyshop_cases_polls';
-    public const CODE_COMPLAINT_CASES_POLLS = 'complaint_cases_polls';
-
-    public const CODE_COLD_CALLS = 'cold_calls';
-    public const CODE_HAS_USERS_SCHEDULE = 'has_users_schedule';
-    public const CODE_MESSAGE_BROADCASTS_EMAIL = 'message_broadcasts_email';
-    public const CODE_MESSAGE_BROADCASTS_SMS = 'message_broadcasts_sms';
-
-    public const CODE_HAS_DNM_WAREHOUSE_STOCK_LOAD = 'has_dnm_warehouse_stock_load';
-    public const CODE_HAS_WAREHOUSE_FILE_STOCK_LOAD = 'has_warehouse_file_stock_load';
-    public const CODE_HAS_WAREHOUSE_MANUAL_CREATED_VEHICLES = 'has_warehouse_manual_created_vehicles';
+    public const CODE_SALES_USED_WAREHOUSE_EXPORTS = 'sales_used_warehouse_exports';
+    public const CODE_CM_EXPERT_USED_WAREHOUSE = 'code_cm_expert_used_warehouse';
+    public const CODE_ACCEPTANCE_VEHICLES_THROUGH_CASES = 'code_acceptance_vehicles_through_cases';
 }
