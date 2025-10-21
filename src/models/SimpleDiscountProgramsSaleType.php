7479b58fae6732dd07dcdfee518602675d650ef3
kuftyrev <kuftyrev.egor@gmail.com>
2025-10-21 17:48:01 +0300

    issue autocrm#18561

---
+++ src/models/SimpleDiscountProgramsSaleType.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace infotech\components\models;
+
+class SimpleDiscountProgramsSaleType
+{
+    public const SALE_TYPE_CASH = 1;
+    public const SALE_TYPE_CREDIT = 2;
+    public const SALE_TYPE_LEASING = 3;
+    public const SALE_TYPE_TRADE_IN = 4;
+}
